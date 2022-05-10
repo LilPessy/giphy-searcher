5604a4d2f0110060cf7b5f6bd3c8f6824e019577
LilPessy <pettidaniele1f@gmail.com>
2022-05-10 20:52:55 +0200

    primo (e forse ultimo) commmit
    funziona tutto

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,17 +20,69 @@ btn.addEventListener("click", function(e){
                 for(let i = 0; i < data.data.length; i++){
                     let gif = document.createElement("div");
                     gif.classList.add("gif");
+
+                    let textContainer = document.createElement("div");
+                    textContainer.classList.add("textContainer");
+                    let title = document.createElement("h2");
+                    title.appendChild(document.createTextNode(data.data[i].title));
+                    textContainer.appendChild(title);
+                    let user = document.createElement("h3");
+                    user.appendChild(document.createTextNode("uploaded by" + data.data[i].username));
+                    textContainer.appendChild(user);
+                    let date = document.createElement("h4");
+                    date.appendChild(document.createTextNode("on " + data.data[i].import_datetime));
+                    textContainer.appendChild(date);
+                    gif.appendChild(textContainer);
+
                     let img = document.createElement("img");
                     let iconContainer = document.createElement("div");
                     iconContainer.classList.add("iconContainer");
                     img.src = data.data[i].images.preview_webp.url;
+                    img.alt = data.data[i].title;
                     gif.appendChild(img);
+
+                    let copyContainer = document.createElement("div");
+                    copyContainer.classList.add("copyContainer");
                     let link = document.createElement("img");
+
+                    let modal = document.createElement("div");
+                    modal.classList.add("modal");
+                    modal.classList.add("hidden");
+                    modal.appendChild(document.createTextNode("L'agg copiat"));
+                    copyContainer.appendChild(modal);
+
+                    link.addEventListener("click", function (e) { 
+                        e.preventDefault();
+                        let text = data.data[i].url;
+                        navigator.clipboard.writeText(text);
+                        modal.classList.remove("hidden");
+                        modal.classList.add("fadeIn");
+                        setTimeout(function () {
+                            modal.classList.add("fadeOut");
+                            setTimeout(function () {
+                                modal.classList.add("hidden");
+                                modal.classList.remove("fadeIn");
+                                modal.classList.remove("fadeOut");
+                            }, 2000);
+                        }, 2000);
+                        
+                        
+                    });
                     link.src = "./assets/img/link.png";
+                    copyContainer.appendChild(link);
+
+                    let codeLink = document.createElement("a");
+                    codeLink.href = data.data[i].url; 
+                    codeLink.target = "_blank";
+                    let codeContainer = document.createElement("div");
+                    codeContainer.classList.add("codeContainer");
                     let code = document.createElement("img");
                     code.src = "./assets/img/code.png";
-                    iconContainer.appendChild(link);
-                    iconContainer.appendChild(code);
+                    codeLink.appendChild(code);
+                    codeContainer.appendChild(codeLink);
+
+                    iconContainer.appendChild(copyContainer);
+                    iconContainer.appendChild(codeContainer);
                     gif.appendChild(iconContainer);
                     container.appendChild(gif);
                 }
